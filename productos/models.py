d52f175433a173b2477399d52af55059c8e589a3
pitin <pitinix@ubuntu.localdomain>
2014-04-29 18:32:43 -0400

    cambios

--- productos/models.py
+++ productos/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from settings import MEDIA_ROOT
 #from django.conf.global_settings import MEDIA_ROOT
-
+from formatChecker import ContentTypeRestrictedFileField
 
 
 ESTADO = (
@@ -25,10 +25,13 @@ class Producto(models.Model):
     #fotografia = models.ImageField
     valor = models.IntegerField(default=0)
     estado = models.CharField(max_length=200,choices=ESTADO)
-    user = ForeignKey(User,  unique=True) 
+    usuario = ForeignKey(User,  unique=True) 
     #fecha = models.DateTimeField('fecha de publicacion del producto', null=False) 
     fecha = models.DateTimeField(_("Date"), auto_now_add=True, null=False)
     fotografia = models.FileField(upload_to="images/", null=True)
+    fotografia2 = models.ImageField("Profile Pic", upload_to="images/", blank=True, null=True)
+    #handout = ContentTypeRestrictedFileField(upload_to='uploads/', content_types=['video/x-msvideo', 'application/pdf', 'video/mp4', 'audio/mpeg', ],max_upload_size=5242880,blank=True, null=True)
+    
     #file = models.FileField(upload_to=MEDIA_ROOT, null=True)
     
     def __unicode__(self):
