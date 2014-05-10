64297ccae67e7a9129d722fb15a0070e93d6c4fe
pitin <pitinix@localhost.localdomain>
2014-05-10 01:46:00 -0400

    cambios!

--- wsgi.py
+++ wsgi.py
@@ -13,16 +13,44 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+#import os
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+
+#!/usr/bin/python
+#import os, sys
+#sys.path.append('/var/www/django/test')
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
+
+
+
+import os
+import sys
+
+# activate venv
+#activate_this = ''
+
+path_to_my_site = '/var/www/django/env/'
+activate_this = os.path.join(path_to_my_site, "bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
+
+# insert project path to sys path
+path = '/var/www/django/test/'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+import django.core.handlers.wsgi
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+application = django.core.handlers.wsgi.WSGIHandler()
